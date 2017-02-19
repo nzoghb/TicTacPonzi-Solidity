dec9f3b73ded0c34cc3912d67ef4b364efee9b48
Nick Zoghb <nzoghb@berkeley.edu>
2017-02-18 22:05:12 -0800

    Major Initial Draft
    Built on previous skeleton code. Added play functionality and various
    checks. Formatting and style improved. Aiming to tackle implementing a
    time limit next.

--- TicTacPonzi.js
+++ TicTacPonzi.js
@@ -6,97 +6,201 @@ pragma solidity ^0.4.0;
 
 contract TicTacPonzi {
 
-	address payee;
-	address challenger;
-	mapping (address => uint) public balances;
-	Game current_game;
+    // PREAMBLE
 
-	// Events to be called when the Game begins and ends.
-	// Used in: start(), check().
-	event gameStart(address first, address second);
-	event gameEnd(address first, address second, uint outcome,
-		uint payee_profit);
+    mapping (address => uint) public balances;
+    Game current_game;
 
-	// Make sure the caller is inputting a positive amount of money into the
-	// pot.
-	// Used in: start(), challenge().
-	modifier hasValue { if(msg.value > 0) _ }
+    /*
+    Events to be called when the Game begins and ends.
+    Used in: TicTacPonzi(), check().
+    */
+    event gameStart(address first, address second);
+    event gameEnd(address first, address second, uint outcome,
+        uint payee_profit);
+    event playerWithdrawal(address player, uint amount);
 
-	// Use this mod on functions that require msg to be the current Payee.
-	// Used in: .
-	modifier isPayee { if (msg.sender == payee) _ }
+    /*
+    Make sure certain conditions are met before executing functions.
+    Used in: TicTacPonzi(), challenge().
+    */
+    modifier hasValue { if(msg.value > 0) _ }
 
-	// Make sure there is only one game active at any one time.
-	// Used in: start().
-	modifier issueGame { if (current_game.active == false) _ }
-
-	// Make sure there is a Payee to challenge.
-	// Used in: challenge().
-	modifier issueChallenge { if (current_game.num_players == 1) _ }
-
-	// The board is a two-dimensional array/mapping, which works best?
-	struct Game {
-		bool active = false;
-        uint pot;
-        uint turn; 
+    /*
+    Data structure representing the game. Contains all temporary variables.
+    */
+    struct Game {
+        bool active;
         address payee;
-        address challenger
+        address challenger;
+        uint pot;
+        uint turn;
         uint num_players;
         uint time_limit;
         mapping(uint => mapping(uint => uint)) board;
-        // private uint[][] board = []
     }
 
+    // CONSTRUCTOR
 
+    /*
+    Only Payee can start a game and must put in a positive amount of money
+    in the pot
+    */
+    function TicTacPonzi() hasValue gameInactive {
+        clear(msg.sender)
+    }
 
-    // Only Payee can start a game and must put in a positive amount of money
-    // in the pot
-    function TicTacPonzi() hasValue issueGame {
-        if (!current_game.length) {
-        	payee = msg.sender;
-        	current_game = Game(True, msg.value, 0, msg.sender, 0, 1, 60, ...);
-        	current_game.active = true
-            msg.value = 0;
-        	}
+    // LOGISTICS-RELATED FUNCTIONS
+
+    /*
+    Only the Challenger can challenge a Payee and must pay in x1.1 whatever
+    the Payee put in the pot.
+    */
+    function challenge() has_value {
+        if (msg.sender != payee) && (current_game.challenger == 0) && 
+            (current_game.active == false) && (current_game.players == 1) {
+            current_game.challenger = msg.sender;
+            gameStart(current_game.payee, current_game.challenger);
         }
     }
 
-    // Only the Challenger can challenge a Payee and must pay in x1.1 whatever
-    // the Payee put in the pot.
-    function challenge() has_value issueChallenge {
-    	if (msg.sender != payee)
-    		challenger = msg.sender;
-    		gameStart(payee, challenger);
-    }
-
-    // Allows current players to inset their tokens onto the board.
-    // Payee tokens are represented as '0' and Challenger tokens as 
-    // '1'.
-    function play(address player, uint row, uint column) {
-        if (msg.sender == payee) && (current_game.turn == 0) {
-        	if check(0, row, column) {
-
-        	}
-        	current_game.turn = 1;
+    /*
+    Converts player addresses into game tokens.
+    Returns 0 if there is an error.
+    */
+    function getPlayerToken(address player) returns (uint token) {
+        if (player == current_game.payee) {
+            return 1;
         }
-        if (msg.sender == challenger) && (current_game.turn == 1) {
-        	if check(1, row, column) {
+        if (player == current_game.challenger) {
+            return -1;
+        }
+        return 0;
+    }
 
-        	}
-        	current_game.turn = 1;
+    /*
+    Converts game tokens into player addresses.
+    Returns 0 if there is an error.
+    */
+    function getPlayerAddress(uint token) returns (address player) {
+        if (token == 1) {
+            return current_game.payee;
+        }
+        if (token == -1) {
+            return current_game.challenger;
+        }
+        return 0;
+    }
+
+    /*
+    Check if a move is valid and whether that move wins the game.
+    Returns a Boolean value.
+    */
+    function checkValidMove(uint token, uint row, uint column)
+        returns (bool valid) {
+        if (row < 0) || (row > 2) || (column < 0) || (column > 2) {
+            return false;
+        }
+        if (board[row][column] != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    Check if a move results in the victory of current player.
+    Returns TRUE if there is a win, FALSE if there is not.
+    */
+    function checkWin(uint player_token) returns (bool win_check){
+        //Checks for the row sums
+        if ((board[0][0] + board[0][1] + board[0][2] == 3 * player_token) ||
+            (board[1][0] + board[1][1] + board[1][2] == 3 * player_token) ||
+            (board[0][0] + board[1][0] + board[2][0] == 3 * player_token)) {
+            return true;
+        }
+        //Checks for the column sums
+        else if ((board[0][0] + board[1][0] + board[2][0] == 3 * player_token) ||
+            (board[0][1] + board[1][1] + board[2][1] == 3 * player_token) ||
+            (board[0][2] + board[1][2] + board[2][2] == 3 * player_token)) {
+            return true;
+        }
+        //Checks diagonal sums
+        else if ((board[0][0] + board[1][1] + board[2][2] == 3 * player_token) ||
+            (board[0][2] + board[1][1] + board[2][0] == 3 * player_token)) {
+            return true;
+        }
+        return false;
+
+    /*
+    Called when a player runs out of time.
+    */
+    function timeOut(address victor) {
+        // mechanism to check time.
+        // winner(victor);
+    }
+
+    // GAME OVER
+
+    /*
+    Is called when a winner is decided. Handles balances and resets the board.
+    Called by: timeOut(), play().
+    */
+    function winner(address victor) {
+
+    }
+
+    // BOARD-RELATED FUNCTIONS
+
+    /*
+    Wipe board and start anew.
+    Called by: TicTacPonzi().
+    */
+    function newBoard(address host) {
+        current_game.active = false;
+        current_game.payee = host;
+        current_game.challenger = 0;
+        current_game.pot = msg.value;
+        current_game.turn = 0;
+        current_game.num_players = 1;
+        current_game.time_limit = 3600;
+
+        for(uint r; r < 3; r++)
+            for(uint c; c < 3; c++)
+                g.board[r][c] = 0;
+    }
+
+    /*
+    Allows current players to inset their tokens onto the board.
+    Payee tokens are represented as '1' and Challenger tokens as 
+    '-1'.
+    */
+    function play(uint row, uint column) {
+        p = getPlayerToken(msg.sender)
+        if (p == 1) && (p == current_game.turn) &&
+            checkValidMove(1, row, column) {
+            board[row][column] = 1;
+            if (checkWin) {
+                winner(current_game.payee);
+            }
+            current_game.turn = -1;
+        }
+        if (p == -1) && (p == current_game.turn) && 
+            checkValidMove(-1, row, column) {
+            board[row][column] = -1;
+            if (checkWin) {
+                winner(current_game.challenger);
+            }
+            current_game.turn = 1;
         }
     }
 
-    // Check if a move is valid and whether that move wins the game.
-    function check(uint token, uint row, uint column) {
-
-    }
-
-    // Allow anyone to check on the state of the game and the moves everyone
-    // has made so far.
-    function get_state(address host) returns (uint _pot, uint _turn,
-    	address _payee, address _challenger, uint _time_limit, uint row1,
-    	uint row2, uint row3) {
+    /*
+    Allow anyone to check on the state of the game and the moves everyone
+    has made so far.
+    */
+    function getState(address host) returns (uint _pot, uint _turn,
+        address _payee, address _challenger, uint _time_limit, uint row1,
+        uint row2, uint row3) {
         _pot = current_game.pot;
         _opposition = current_game.challenger;
         _time_limit = current_game.time_limit;
@@ -108,5 +212,4 @@ contract TicTacPonzi {
         row3 = (100 * (current_game.board[2][0] + 1)) +
         (10 * (current_game.board[2][1])) + (current_game.board[2][2]);
     }
-
 }
