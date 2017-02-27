48ff19569116d980d81171df8ef35bbcbe51f3ea
Nick Zoghb <nzoghb@berkeley.edu>
2017-02-26 19:11:26 -0800

    Finished Product
    Final deliverable, ponzi scheme fully functional and tested.

--- TicTacPonzi.js
+++ TicTacPonzi.js
@@ -1,7 +1,7 @@
 //	TicTacPonzi Contract for Blockchain@Berkeley.
 //	Copyright (c) 2017 Nicolas Zoghb (IoT Agriculture Team).
 //  Released under the MIT License.
-//	Version 1.2.0, 23 February 2017 (Started: 15 February 2017).
+//	Version 1.3.0, 26 February 2017 (Started: 15 February 2017).
 
 pragma solidity ^0.4.0;
 
@@ -11,41 +11,49 @@ contract TicTacPonzi {
 
 	mapping (address => uint) public balances;
 	Game private current_game;
+	address private payee;
+	address private challenger;
+	uint private payee_payment;
+	uint private challenger_payment;
 
 	/*
 	Make sure certain conditions are met before executing functions.
-	Used in: TicTacPonzi(), challenge().
+	Used in: TicTacPonzi(), start(), challenge().
 	*/
-	modifier hasValue { if (msg.value > 0) _; }
-	modifier gameActive (bool _active) 
-		{ if (current_game.active == _active) _; }
-	modifier numPlayers (int num)
-		{ if (current_game.num_players == num) _; }
+	modifier hasValue {
+		if (msg.value <= 0) {
+			throw;
+		}
+		_;
+	}
+
+	modifier numPlayers (int num) {
+		if (current_game.num_players != num) {
+			throw;
+		}
+		_;
+	}
 
 	/*
 	Data structure representing the game. Contains all temporary variables.
 	*/
 	struct Game {
 		bool active;
-		address payee;
-		address challenger;
-		uint old_pot;
-		uint pot;
 		int turn;
 		int num_players;
-		uint time_limit;
+		uint last_time;
 		mapping(uint => mapping(uint => int)) board;
+		uint turns_played;
 	}
 
 	//	CONSTRUCTOR
 
 	/*
-	Only Payee can start a game and must put in a positive amount of money
-	in the pot
+	Only Payee can start a game and must put in a positive amount of money in
+	the pot.
 	*/
 	function TicTacPonzi() {
 		newBoard(msg.sender);
-		current_game.pot = 0;
 	}
 
 	//	LOGISTICS-RELATED FUNCTIONS
@@ -55,18 +63,17 @@ contract TicTacPonzi {
 	contract has just been deployed OR if there is no current Payee (last
 	Payee forfeited).
 	*/
-	function start() payable gameActive(false) numPlayers(0) hasValue public {
-		if (msg.value >= current_game.pot) {
-			if (msg.sender == current_game.payee) {
-				balances[msg.sender] = msg.value;
+	function start() payable numPlayers(0) hasValue public {
+		balances[msg.sender] += msg.value;
+		if (msg.value >= payee_payment) {
+			if (msg.sender == payee) {
+				payee_payment = msg.value;
 				current_game.num_players = 1;
-				current_game.pot = msg.value;
-				current_game.old_pot = current_game.pot;
 			}
-			else if (current_game.payee == 0) {
-				current_game.payee = msg.sender;
-				current_game.pot = msg.value;
-				current_game.old_pot = current_game.pot;
+			else if (payee == 0) {
+				payee = msg.sender;
+				payee_payment = msg.value;
+				current_game.num_players = 1;
 			}
 		}
 	}
@@ -76,17 +83,16 @@ contract TicTacPonzi {
 	the Payee put in the pot.
 	*/
 	function challenge() payable numPlayers(1) hasValue public {
-		balances[msg.sender] = msg.value;
-		if ((msg.sender != current_game.payee) &&
-			(current_game.challenger == 0) &&
-			(msg.value >= (11*current_game.old_pot)/10)) {
-			current_game.challenger = msg.sender;
+		if (msg.sender == payee) {
+			throw;
+		}
+		balances[msg.sender] += msg.value;
+		if (challenger == 0 && msg.value >= uint(11*int(payee_payment)/10)) {
+			challenger = msg.sender;
 			current_game.num_players = 2;
-			current_game.active = true;
-			current_game.old_pot = current_game.pot;
-			current_game.pot = msg.value;
+			challenger_payment = msg.value;
 			current_game.turn = 1;
-			current_game.time_limit = now;
+			current_game.last_time = now;
 		}
 	}
 
@@ -95,10 +101,10 @@ contract TicTacPonzi {
 	Returns 0 if there is an error.
 	*/
 	function getPlayerToken(address player) private returns (int) {
-		if (player == current_game.payee) {
+		if (player == payee) {
 			return 1;
 		}
-		else if (player == current_game.challenger) {
+		else if (player == challenger) {
 			return -1;
 		}
 		return 0;
@@ -110,10 +116,10 @@ contract TicTacPonzi {
 	*/
 	function getPlayerAddress(int token) private returns (address) {
 		if (token == 1) {
-			return current_game.payee;
+			return payee;
 		}
 		else if (token == -1) {
-			return current_game.challenger;
+			return challenger;
 		}
 		return 0;
 	}
@@ -123,8 +129,7 @@ contract TicTacPonzi {
 	Returns a Boolean value.
 	Called by: play().
 	*/
-	function checkValidMove(int token, uint row, uint column)
-		gameActive(true) private returns (bool) {
+	function checkValidMove(uint row, uint column) private returns (bool) {
 		if ((row < 0) || (row > 2) || (column < 0) || (column > 2)) {
 			return false;
 		}
@@ -137,9 +142,10 @@ contract TicTacPonzi {
 	/*
 	Check if a move results in the victory of current player.
 	Returns TRUE if there is a win, FALSE if there is not.
+	Loops are rows, columns, diagonals in that order.
 	Called by: play().
 	*/
-	function checkWin(int player_token) gameActive(true) private returns (bool) {
+	function checkWin(int player_token) private returns (bool) {
 		//	Checks for the row sums
 		if ((current_game.board[0][0] + current_game.board[0][1] +
 			current_game.board[0][2] == 3 * player_token) ||
@@ -172,33 +178,23 @@ contract TicTacPonzi {
 	Checks the board for a draw.
 	Called by: play().
 	*/
-	function checkDraw() gameActive(true) private returns (bool) {
-		for(uint row; row < 3; row++) {
-			for(uint column; column < 3; column++) {
-				if (current_game.board[row][column] == 0) {
-					return false;
-				}
-			}
+	function checkGameEnd() private returns (bool) {
+		if (current_game.turns_played == 9) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 	/*
 	Checks when a player runs out of time.
 	Called by: play().
 	*/
-	function checkTimeOut() gameActive(true) public {
-		if (now - current_game.time_limit > 3600) {
+	function checkTimeOut() numPlayers(2) public returns (bool) {
+		if (now - current_game.last_time > 3600) {
 			winner(getPlayerAddress(-1*current_game.turn));
+			return true;
 		}
-		updateTimeLimit();
-	}
-
-	/*
-	Update the time limit every time a function is called.
-	*/
-	function updateTimeLimit() private {
-		current_game.time_limit = now;
+		return false;
 	}
 
 	//	GAME OVER
@@ -208,29 +204,28 @@ contract TicTacPonzi {
 	Called by: timeOut(), forfeit(), play().
 	*/
 	function winner(address victor) private {
-		uint _pot = balances[current_game.payee] +
-				balances[current_game.challenger];
-		if (victor == current_game.payee) {
-			balances[current_game.payee] = _pot;
-			balances[current_game.challenger] = 0;
+		if (victor == payee) {
+			balances[challenger] -= challenger_payment;
+			balances[payee] += challenger_payment;
 		}
-		else if (victor == current_game.challenger) {
-			balances[current_game.payee] = current_game.old_pot;
-			balances[current_game.challenger] = _pot - current_game.old_pot;
+		else if (victor == challenger) {
+			balances[challenger] -= payee_payment;
+			balances[payee] += payee_payment;
 		}
-		newBoard(current_game.challenger);
+		newBoard(challenger);
+		payee_payment = challenger_payment;
+		challenger_payment = 0;
 		current_game.num_players = 1;
 	}
 
 	/*
 	Player forfeits to their opponent.
 	*/
-	function forfeit() gameActive(true) public {
-		if (msg.sender == current_game.payee || 
-			msg.sender == current_game.challenger) {
+	function forfeit() public {
+		if (msg.sender == payee || 
+			msg.sender == challenger) {
 			if (current_game.num_players == 1) {
 				newBoard(0);
-				current_game.active = false;
 			}
 			else if (current_game.num_players == 2) {
 				winner(getPlayerAddress(-1*getPlayerToken(msg.sender)));
@@ -242,8 +237,8 @@ contract TicTacPonzi {
 	Used by players to withdraw their money from their account.
 	*/
 	function withdraw() public returns (bool) {
-		if ((msg.sender == current_game.payee) ||
-			(msg.sender == current_game.challenger)) {
+		if ((msg.sender == payee) ||
+			(msg.sender == challenger)) {
 				return false;
 		}
 		uint amount = balances[msg.sender];
@@ -261,50 +256,84 @@ contract TicTacPonzi {
 
 	/*
 	Wipe board and start anew.
-	Called by: TicTacPonzi(), join(), winner(), forfeit().
+	Called by: TicTacPonzi(), winner(), forfeit().
 	*/
 	function newBoard(address host) private {
-		current_game.active = true;
-		current_game.payee = host;
-		current_game.challenger = 0;
-		current_game.turn = 0;
-		current_game.num_players = 0;
-		current_game.time_limit = 0;
+		payee = host;
+		challenger = 0;
+		delete(current_game);
+
+		current_game.board[0][0] = 0;
+		current_game.board[0][1] = 0;
+		current_game.board[0][2] = 0;
+		current_game.board[1][0] = 0;
+		current_game.board[1][1] = 0;
+		current_game.board[1][2] = 0;
+		current_game.board[2][0] = 0;
+		current_game.board[2][1] = 0;
+		current_game.board[2][2] = 0;
 	}
 
 	/*
-	Allows current players to inset their tokens onto the board.
+	Allow current players to insert their tokens onto the board.
 	Payee tokens are represented as '1' and Challenger tokens as 
 	'-1'.
 	*/
-	function play(uint row, uint column) gameActive(true) public {
-		checkTimeOut();
+	function play(uint row, uint column) numPlayers(2) public {
+		if (checkTimeOut()) {
+			return;
+		}
 		int p = getPlayerToken(msg.sender);
-
 		if ((p == current_game.turn) && 
-			checkValidMove(p, row, column)) {
+			checkValidMove(row, column)) {
 			current_game.board[row][column] = p;
+			current_game.last_time = now;
+			current_game.turns_played += 1;
 			if (checkWin(p)) {
 				winner(getPlayerAddress(p));
+				return;
 			}
-			else if (checkDraw()) {
-				winner(current_game.payee);
+			else if (checkGameEnd()) {
+				winner(payee);
+				return;
 			}
 			current_game.turn = -1 * p;
 		}
 	}
 
 	/*
+	Allow anyone to check their funds.
+	*/
+	function getBalance(address host) constant public returns (uint) {
+		return balances[host];
+	}
+
+	/*
+	Allow anyone to check how much time is left before the turn ends.
+	*/
+	function getTimeLeft() numPlayers(2) constant public returns (uint) {
+		uint diff = now - current_game.last_time;
+		if (diff >= 3600) {
+			return 0;
+		}
+		return 3600 - diff;
+	}
+
+	/*
 	Allow anyone to check on the state of the game and the moves everyone
 	has made so far.
 	*/
-	function getState(address host) gameActive(true) constant public returns
-		(uint _pot, int _turn, address _payee, address _challenger,
-			uint _time_limit, int[3] row1, int[3] row2, int[3] row3) {
-		_pot = current_game.pot;
-		_challenger = current_game.challenger;
-		_time_limit = current_game.time_limit;
+	function getState() constant public returns
+		(address _payee, uint _payee_payment, address _challenger,
+			uint _challenger_payment,int _turn, uint _last_time,
+			int _num_players, int[3] row1, int[3] row2, int[3] row3) {
+		_payee = payee;
+		_payee_payment = payee_payment;
+		_challenger = challenger;
+		_challenger_payment = challenger_payment;
 		_turn = current_game.turn;
+		_last_time = current_game.last_time;
+		_num_players = current_game.num_players;
 		row1 = [current_game.board[0][0], current_game.board[0][1],
 		current_game.board[0][2]];
 		row2 = [current_game.board[1][0], current_game.board[1][1],
